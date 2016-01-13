4da92218dd68213906c1e4edd5274930005cd1a3
Roman Frołow <rofrol@gmail.com>
2016-01-13 03:05:17 +0100

    Increment/Decrement

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,14 @@
-import Html exposing (text, div, ul, li)
+import Html exposing (text, div, ul, li, button)
 import Markdown
 import String.Interpolate exposing(interpolate)
+import Html.Events exposing (onClick)
+import StartApp.Simple as StartApp
 
-main: Html.Html
-main =
+model: number
+model = 0
+
+view: Signal.Address Action -> a -> Html.Html
+view address model =
   div [] [
     text "Hello, world",
     ul []
@@ -14,7 +19,12 @@ main =
     text (toString (double 2)),
     div [] [ text (toString list) ],
     text a,
-    Html.p [] [text output]
+    Html.p [] [text output],
+    div []
+    [ button [ onClick address Decrement ] [ text "-" ]
+    , div [] [ text (toString model)]
+    , button [ onClick address Increment ] [ text "+" ]
+    ]
   ]
 
 markdown: String
@@ -42,3 +52,14 @@ a = over9000 9001
 output : String
 output =
   interpolate "My name is {0} {1}" ["Luke", "Westby"]
+
+type Action = Increment | Decrement
+
+update: Action -> number -> number
+update action model =
+  case action of
+    Increment -> model + 1
+    Decrement -> model - 1
+
+main: Signal Html.Html
+main = StartApp.start { model = model, view = view, update = update }
