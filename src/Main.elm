92232e6c6f8584e3dc8a010a48917851ec033edc
Roman Frołow <rofrol@gmail.com>
2016-01-13 01:57:51 +0100

    string interpolation

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 import Html exposing (text, div, ul, li)
 import Markdown
+import String.Interpolate exposing(interpolate)
 
 main: Html.Html
 main =
@@ -11,7 +12,9 @@ main =
     ],
     Markdown.toHtml markdown,
     text (toString (double 2)),
-    div [] [ text (toString list) ]
+    div [] [ text (toString list) ],
+    text a,
+    Html.p [] [text output]
   ]
 
 markdown: String
@@ -31,7 +34,11 @@ list = [1, 2, 3 ,4]
 
 over9000: number -> String
 over9000 powerLevel =
-  if powerLevel > 9000 then "upps 9000" else "meh"
+  if powerLevel > 9000 then "Ups, over 9000!!!" else "meh"
 
 a : String
 a = over9000 9001
+
+output : String
+output =
+  interpolate "My name is {0} {1}" ["Luke", "Westby"]
